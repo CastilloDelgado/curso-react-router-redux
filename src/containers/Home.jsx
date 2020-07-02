de1b5aeb0a24dd3dfe2e67c60b36b3f847087f0f
Marco Castillo <marco.castillo@kronomx.com>
2020-07-02 11:01:33 -0500

    Creacion de reducers y actions

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,45 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import Search from '../components/Search';
-import Categories from '../components/Categories';
-import Carousel from '../components/Carousel';
-import CarouselItem from '../components/CarouselItem';
-import useInitialState from '../hooks/useInitialState';
-import '../assets/styles/App.scss';
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import Search from "../components/Search";
+import Categories from "../components/Categories";
+import Carousel from "../components/Carousel";
+import CarouselItem from "../components/CarouselItem";
+import "../assets/styles/App.scss";
 
-const API = 'http://localhost:3000/initalState';
-
-const Home = () => {
-  const initialState = useInitialState(API);
-  return initialState.length === 0 ? (
-    <h1>Loading...</h1>
-  ) : (
-    <React.Fragment>
-      <Search />
-      {initialState.mylist.length > 0 && (
-        <Categories title='Mi Lista'>
-          <Carousel>
-            {initialState.mylist.map((item) => (
-              <CarouselItem key={item.id} {...item} />
-            ))}
-          </Carousel>
-        </Categories>
-      )}
-      <Categories title='Tendencias'>
+const Home = ({ myList, trends, originals }) => (
+  <React.Fragment>
+    <Search />
+    {myList.length > 0 && (
+      <Categories title="Mi Lista">
         <Carousel>
-          {initialState.trends.map((item) => (
+          {myList.map((item) => (
             <CarouselItem key={item.id} {...item} />
           ))}
         </Carousel>
       </Categories>
-      <Categories title='Originales de Platzi Video'>
-        <Carousel>
-          {initialState.originals.map((item) => (
-            <CarouselItem key={item.id} {...item} />
-          ))}
-        </Carousel>
-      </Categories>
-    </React.Fragment>
-  );
+    )}
+    |
+    <Categories title="Tendencias">
+      <Carousel>
+        {trends.map((item) => (
+          <CarouselItem key={item.id} {...item} />
+        ))}
+      </Carousel>
+    </Categories>
+    <Categories title="Originales de Platzi Video">
+      <Carousel>
+        {originals.map((item) => (
+          <CarouselItem key={item.id} {...item} />
+        ))}
+      </Carousel>
+    </Categories>
+  </React.Fragment>
+);
+
+const mapStateToProps = (state) => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals,
+  };
 };
 
-export default Home;
+export default connect(mapStateToProps, null)(Home);
